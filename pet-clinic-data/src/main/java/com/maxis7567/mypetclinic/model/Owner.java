65899a7f1ed962b1ff586e73355a9cc4847ecaeb
mahdisy <mahdi.sy1994@outlook.com>
2020-02-27 22:17:29 +0330

    multi module init

---
+++ pet-clinic-data/src/main/java/com/maxis7567/mypetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.maxis7567.mypetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ pet-clinic-data/src/main/java/com/maxis7567/mypetclinic/model/Person.java
@@ -0,0 +1,23 @@
+package com.maxis7567.mypetclinic.model;
+
+public class Person {
+
+    private String name;
+    private String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
